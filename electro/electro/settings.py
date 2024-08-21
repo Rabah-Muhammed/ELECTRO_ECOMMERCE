0bca9a0c7165bca245b7018b860978737bd7e765
Rabah-Muhammed <rabarabi31@gmail.com>
2024-08-21 14:09:55 +0530

    settings.py changed

--- electro/electro/settings.py
+++ electro/electro/settings.py
@@ -173,6 +173,8 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 #media files configuration
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
